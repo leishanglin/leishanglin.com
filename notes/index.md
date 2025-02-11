297d524b1c18f024989fdffb8208840591cbc602
leishanglin <reasonly8@sina.com>
2025-02-11 18:15:28 +0800

    daily updates

--- notes/index.md
+++ notes/index.md
@@ -1,8 +1,9 @@
 ---
-title: leishanglin(雷尚林) 的个人网站
-keywords: leishanglin, 个人网站, 博客
+title: 首页
+keywords: leishanglin, 个人网站, 首页
 description: 这是 leishanglin 的个人网站，用于个人博客、简历、作品展示等
 ---
-# leishanglin.com
+
+## 关于
 
 这是我的个人网站
--- scripts/build.ts
+++ scripts/build.ts
@@ -6,15 +6,28 @@ import * as fs from 'node:fs/promises';
 import { emptyDir } from 'fs-extra';
 import ejs from 'ejs';
 import { minify } from 'html-minifier-terser';
+import dayjs from 'dayjs';
 
-const filesMap = await loadDir(path.resolve(__dirname, '../notes'));
-const prefixPath = path.resolve(__dirname, '../dist');
+const NOTES_DIR = '../notes';
+const DIST_DIR = '../dist';
+const AUTHOR = 'leishanglin(雷尚林)';
+const DOMAIN_NAME = "leishanglin.com"
+// const DOMAIN = 'https://leishanglin.com';
+const DOMAIN = 'http://127.0.0.1:8083';
+
+const filesMap = await loadDir(path.resolve(__dirname, NOTES_DIR));
+const prefixPath = path.resolve(__dirname, DIST_DIR);
 
 const dirCheckMap: Record<string, boolean> = {};
 
 await emptyDir(prefixPath);
 
 for (const [fileRelativePath, content] of Object.entries(filesMap)) {
+  const fileOriginFullPath = path.resolve(
+    process.cwd(),
+    'notes',
+    fileRelativePath,
+  );
   const fileFullPath = path.resolve(prefixPath, fileRelativePath);
   const fileDir = path.dirname(fileFullPath);
 
@@ -31,13 +44,25 @@ for (const [fileRelativePath, content] of Object.entries(filesMap)) {
       // 添加语义化标签：figure，并让图片进行懒加载（loading="lazy"）
       return `<figure><img src="${image.href}" alt="${image.text}" loading="lazy" /><figcaption>${image.text}</figcaption></figure>`;
     };
-    
+
     marked.setOptions({ renderer });
 
     const rawHtml = marked.parse(dataObj.content, {}) as string;
+    const fileStat = await fs.stat(fileOriginFullPath);
+    const createdAt = fileStat.birthtime;
+    const updatedAt = fileStat.mtime;
     const renderedRawHtml = await ejs.renderFile(
       path.resolve(__dirname, './index.html.ejs'),
-      { content: rawHtml, author: 'leishanglin(雷尚林)', ...dataObj.data },
+      {
+        content: rawHtml,
+        author: AUTHOR,
+        ...dataObj.data,
+        domain: DOMAIN,
+        domainName: DOMAIN_NAME,
+        path: `/${fileRelativePath.replace(/\.md$/, '')}`,
+        datePublished: dayjs(createdAt).format('YYYY-MM-DD'),
+        dateModified: dayjs(updatedAt).format('YYYY-MM-DD'),
+      },
     );
 
     const minifiedRawHtml = await minify(renderedRawHtml, {
